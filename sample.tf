620c6f1d75e84bee0fa659953d5567f13aaae494
MahipalreddyG <37070078+MahipalreddyG@users.noreply.github.com>
2018-03-26 16:22:06 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -3,6 +3,9 @@ provider "aws" {
   secret_key = "**********************"
   region     = "us-west-2"
 }
+
+# Launch AWS instance
+
 resource "aws_instance" "sample" {
   ami="ami-4475eb3c"
   instance_type="t2.micro"
@@ -11,15 +14,24 @@ resource "aws_instance" "sample" {
  tags{
    Name="tf_02"
    }
+
+# Copy the pem key from local to Remote machine
+
 connection {
   user = "ubuntu"
   type = "ssh"
   private_key ="${file("/home/ubuntu/ec2.pem")}"
 }
-provisioner "file" {
+  
+# copy script.sh file from Loacal to remote machine
+  
+  provisioner "file" {
     source      = "/home/ubuntu/tf/script.sh"
     destination = "/tmp/script.sh"
   }
+  
+# Give the execute permission to script.sh and give full permissions to /var /lib/tomcat7/webapps folder
+  
  provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/script.sh",
@@ -27,10 +39,16 @@ provisioner "file" {
       "sudo chmod 777 /var/lib/tomcat7/webapps/"
     ]
   }
+  
+#Copy the war file from Local machine to remote machine tomcat webapps folder
+  
  provisioner "file" {
     source      = "/home/ubuntu/sample.war"
     destination = "/var/lib/tomcat7/webapps/sample.war"
   }
+  
+# Restart the tomcat 
+ 
  provisioner "remote-exec" {
     inline = [
       "sudo service tomcat7 restart"
